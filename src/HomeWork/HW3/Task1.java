4f2192f4bc81c969855ba04a894cb8352c34fd59
HlebSamoilau <Glebsamoilov@gmail.com>
2019-04-21 21:42:32 +0300

    Commit3

--- src/HomeWork/HW3/Task1.java
+++ src/HomeWork/HW3/Task1.java
@@ -1,6 +1,13 @@
 package HomeWork.HW3;
 
 public class Task1 {
+    public static void main(String[] args) {
+        double number1 = 0.0039;
+        int number2 = Integer.parseInt(args[0]);
+        double answer = number2/number1;
+        System.out.println(answer);
 
+
+    }
 
     }
