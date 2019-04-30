b3a3d95ac9be5259d92abb3bbf4ea9324768861c
szakrevskaya <selina.88@mail.ru>
2019-04-30 11:50:04 +0300

    Update Task2.java

--- src/Test1/Task2.java
+++ src/Test1/Task2.java
@@ -2,7 +2,7 @@ package Test1;
 
 
 import java.util.Arrays;
-
+//Не забывай про форматирование кода: ctrl + alt + l в идее
 public class Task2 {
     public static void main(String[] args) {
         int[]array = new int[15];
